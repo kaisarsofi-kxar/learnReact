5ac81dc5d1c05bd6191f4667fc1722f80e2e6191
kaisarsofi-kxar <kaisersofi.ks2515@gmail.com>
2023-10-12 14:37:27 +0530

    this is second

---
+++ 01backgroundChanger/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <>
+      <div className="w-full h-screen duration-200"></div>
+    </>
+  );
+}
+
+export default App;
