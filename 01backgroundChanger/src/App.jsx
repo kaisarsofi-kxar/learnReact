52c38380847e5188116058a9ef65fd0844dc2999
kaisarsofi-kxar <kaisersofi.ks2515@gmail.com>
2023-10-14 10:36:15 +0530

    this is the password genarator

--- 01backgroundChanger/src/App.jsx
+++ 01backgroundChanger/src/App.jsx
@@ -1,11 +1,118 @@
 import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("olive");
+  const [color, setColor] = useState("Red");
 
   return (
     <>
-      <div className="w-full h-screen duration-200"></div>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 ">
+          <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-2 rounded-3xl">
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{
+                backgroundColor: "red",
+              }}
+              onClick={() => setColor("red")}
+            >
+              Red
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{
+                backgroundColor: "Green",
+              }}
+              onClick={() => setColor("green")}
+            >
+              Green
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{
+                backgroundColor: "Blue",
+              }}
+              onClick={() => setColor("blue")}
+            >
+              Blue
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{
+                backgroundColor: "olive",
+              }}
+              onClick={() => setColor("olive")}
+            >
+              Olive
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{
+                backgroundColor: "gray",
+              }}
+              onClick={() => setColor("gray")}
+            >
+              Gray
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+              style={{
+                backgroundColor: "yellow",
+              }}
+              onClick={() => setColor("yellow")}
+            >
+              Yellow
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+              style={{
+                backgroundColor: "pink",
+              }}
+              onClick={() => setColor("pink")}
+            >
+              Pink
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{
+                backgroundColor: "purple",
+              }}
+              onClick={() => setColor("purple")}
+            >
+              Purple
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-c shadow-lg"
+              style={{
+                backgroundColor: "Lavender",
+              }}
+              onClick={() => setColor("Lavender")}
+            >
+              Lavender
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+              style={{
+                backgroundColor: "white",
+              }}
+              onClick={() => setColor("white")}
+            >
+              White
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{
+                backgroundColor: "Black",
+              }}
+              onClick={() => setColor("Black")}
+            >
+              White
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ 02passwordGenerator/src/App.jsx
@@ -0,0 +1,92 @@
+import { useState, useCallback, useEffect, useRef } from "react";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //ref hook
+  const passwordRef = useRef(null);
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (characterAllowed) str += "!@#$%^&*()+={}[]~`";
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, characterAllowed, setPassword]);
+  const copyPassToClipBoard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 100);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, characterAllowed, passwordGenerator]);
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-blue-500"
+            onClick={copyPassToClipBoard}
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label>Number</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={characterAllowed}
+              id="numberInput"
+              onChange={() => {
+                setCharacterAllowed((prev) => !prev);
+              }}
+            />
+            <label>Charecter</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
